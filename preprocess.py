258412b38f9d27b665a2bd8a0b67ecf95a530c47
RavibhaiDhola <162813259+RavibhaiDhola@users.noreply.github.com>
2024-03-09 15:53:12 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,17 +2,17 @@ import cv2
 
 # to convert images from RGB to Grayscale
 def grayscale(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return img
+    im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return im
     
 # To equalize the intensity of converted grayscale image
 def equalize(img):
-    img = cv2.equalizeHist(img)
-    return img
+    im = cv2.equalizeHist(img)
+    return im
     
 # Preprocess the image and normalization of image
 def preprocess(img):
-    img = grayscale(img)
-    img = equalize(img)
-    img = img/255
-    return img
+    im = grayscale(img)
+    im = equalize(img)
+    im = im/255
+    return im
